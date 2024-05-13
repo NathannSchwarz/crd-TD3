dfa978c7d8b63804a4eb2c8619ac0167b3a6fad2
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2024-05-13 11:17:10 +0200

    modification

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import Hero from '@/components/Hero.vue'
+import Card from '@/components/Card.vue'
 </script>
 <template>
   <Hero title="Agenda" imgPath="./img/orchestre.webp" imgAlt="orchestre">
     <template #chapo>
-      <p>
+      <p class="text-xl leading-normal lg:text-3xl">
         Lieu de création artistique, le conservatoire propose régulièrement des actions culturelles
         ouvertes à tous.
       </p>
     </template>
   </Hero>
+  <div>
+    <Card />
+  </div>
 </template>
